088206caaba0edb5798b79697f1ea33703c9b9de
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-06-08 16:38:57 -0300

    feat: data mining from the website

--- scraper.js
+++ scraper.js
@@ -4,14 +4,30 @@ const { url } = require("./config");
 
 (async () => {
   // open browser
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: 'new' });
   const page = await browser.newPage();
 
   // Setup Browser
   
-  await page.setDefaultTimeout(10000); 
+  await page.setDefaultTimeout(20000); 
   await page.setViewport({ width: 1200, height: 800 })
   await page.setUserAgent(random_useragent.getRandom())
+
+  // Get data from bookstore
+
+  const name_selector = ".product_main > h1"
+  const price_selector = ".price_color"
+  await page.goto(url)
+  await page.waitForSelector(name_selector)
+  await page.waitForSelector(price_selector)
+  const name = await page.$eval(name_selector, e => e.innerHTML)
+  const price = await page.$eval(price_selector, e => e.innerHTML)
+  const nameTrim = name.trim()
+  const priceTrim = price.trim()
+
+  console.log("NAME: " + nameTrim)
+  console.log("PRICE: " + priceTrim)
+
 
   //close browser
   await browser.close();
